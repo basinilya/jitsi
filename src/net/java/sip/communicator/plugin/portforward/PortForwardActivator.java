b34047be69de717c2ffdfd42f4edc82046fad0ff
Ilya Basin <basinilya@gmail.com>
2019-07-13 15:43:05 +0300

    x

--- src/net/java/sip/communicator/plugin/portforward/PortForwardActivator.java
+++ src/net/java/sip/communicator/plugin/portforward/PortForwardActivator.java
@@ -6,6 +6,13 @@ import java.util.Map.Entry;
 import net.java.sip.communicator.impl.protocol.irc.*;
 import net.java.sip.communicator.plugin.irccommands.command.*;
 import net.java.sip.communicator.plugin.irccommands.command.Mode;
+import net.java.sip.communicator.service.contactlist.MetaContact;
+import net.java.sip.communicator.service.contactlist.MetaContactListService;
+import net.java.sip.communicator.service.contactlist.event.MetaContactListListener;
+import net.java.sip.communicator.service.protocol.Contact;
+import net.java.sip.communicator.service.protocol.Message;
+import net.java.sip.communicator.service.protocol.OperationSetBasicInstantMessaging;
+import net.java.sip.communicator.util.ServiceUtils;
 
 import org.osgi.framework.*;
 
@@ -34,7 +41,28 @@ public class PortForwardActivator
      * @param context the bundle context
      */
     @Override
-    public void start(final BundleContext context)
+    public void start(final BundleContext bContext)
     {
+        PortForwardActivator.bundleContext = bContext;
+        MetaContactListService metaCListService = ServiceUtils.getService(
+                bundleContext,
+                MetaContactListService.class);
+        metaCListService.getRoot();
+        // basinilya@jabber.ru-2-zazaza
+        Iterator<MetaContact> it = metaCListService.findAllMetaContactsForAddress("zzzz@zzz.org");
+        for(;it.hasNext();) {
+            MetaContact metaCon = it.next();
+            System.out.println(metaCon.getDisplayName());
+            Contact contact = metaCon.getDefaultContact();
+            OperationSetBasicInstantMessaging imOpSet = contact.getProtocolProvider().getOperationSet(OperationSetBasicInstantMessaging.class);
+            Message message = imOpSet.createMessage("hello");
+            imOpSet.sendInstantMessage(contact, message);
+            System.out.println(contact.getDisplayName());
+            System.out.println(contact.getPresenceStatus().getStatusName());
+            break;
+        }
+        MetaContactListListener lstn;
+       // metaCListService.addMetaContactListListener(l);
     }
+    public static BundleContext bundleContext;
 }
