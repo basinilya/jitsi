4d166c83778c0763a83557436b1909ee0f5d98cb
Ilya Basin <basinilya@gmail.com>
2019-07-17 20:01:54 +0300

    x

--- src/net/java/sip/communicator/plugin/portforward/PropfileUtils.java
+++ src/net/java/sip/communicator/plugin/portforward/PropfileUtils.java
@@ -18,10 +18,13 @@ public class PropfileUtils
 
     private static final String PREFIX = "portforward.";
 
-    public static Set<String> listSubKeys(Properties props, String prefix) {
+    public static Set<String> listSubKeys(Properties props, String prefix)
+    {
         Set<String> res = new HashSet<>();
-        for (String x : props.stringPropertyNames()) {
-            if (x.startsWith(prefix)) {
+        for (String x : props.stringPropertyNames())
+        {
+            if (x.startsWith(prefix))
+            {
                 String[] a = x.substring(prefix.length()).split("[.]", 2);
                 String key = a[0];
                 res.add(key);
@@ -30,18 +33,84 @@ public class PropfileUtils
         return res;
     }
 
+    private Properties props;
+
+    public void load() throws Exception
+    {
+        props = new Properties();
+        try (FileInputStream in = new FileInputStream(
+            "C:/progs/media/jitsi/lib/portforward.properties"))
+        {
+            props.load(in);
+            in.read();
+        }
+    }
+
+    public void start() throws Exception
+    {
+        load();
+        Set<String> forwards = listSubKeys(props, PREFIX);
+        for (String name : forwards)
+        {
+            System.out.println(name);
+            Forward forward = new Forward(name);
+            System.out.println(forward.getContact());
+            System.out.println(forward.getAddress());
+            System.out.println(forward.isListen());
+        }
+    }
+
+    public class Forward
+    {
+        private String name;
+
+        public Forward(String name)
+        {
+            this.name = name;
+        }
+
+        public String getContact()
+        {
+            return getProp("contact");
+        }
+
+        public String getAddress()
+        {
+            return getProp("address");
+        }
+
+        /**
+         * @return should we listen, default true
+         */
+        public boolean isListen()
+        {
+            return !Boolean.FALSE.equals(getBoolean("listen"));
+        }
+
+        /**
+         * 
+         * @param prop
+         * @return true or false if and only if the string value is exactly
+         *         "true" or "false", otherwise null
+         */
+        public Boolean getBoolean(String prop)
+        {
+            String s = getProp(prop);
+            return Boolean.toString(false).equalsIgnoreCase(s) ? Boolean.FALSE
+                : Boolean.toString(true).equalsIgnoreCase(s) ? Boolean.TRUE : null;
+        }
+
+        public String getProp(String prop)
+        {
+            return props.getProperty(PREFIX + name + "." + prop);
+        }
+
+    }
+
     public static void main(String[] args) throws Exception
     {
-        Properties props = new Properties();
-        props.load(new FileInputStream("C:/progs/media/jitsi/lib/portforward.properties"));
-        Map<String, String> map = new HashMap<>();
-        Set<String> subKeys = listSubKeys(props, PREFIX);
-        for (String subKey : subKeys) {
-            System.out.println(subKey);
-            System.out.println(props.getProperty(PREFIX + subKey + ".contact"));
-            System.out.println(props.getProperty(PREFIX + subKey + ".address"));
-            System.out.println(props.getProperty(PREFIX + subKey + ".listen"));
-        }
+        PropfileUtils inst = new PropfileUtils();
+        inst.start();
         System.exit(0);
         InetSocketAddress xxx = parseAddress0("[::]", 0);
         System.out.println(xxx.getHostName());
